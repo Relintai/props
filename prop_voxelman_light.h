0aceb2aeca8a9d7cd2c312b66cf23dc330f041b2
Relintai <relintai@gmail.com>
2020-04-09 12:36:33 +0200

    Fix build for 4.0.

--- prop_voxelman_light.h
+++ prop_voxelman_light.h
@@ -23,7 +23,15 @@ SOFTWARE.
 #ifndef PROP_VOXELMAN_LIGHT_H
 #define PROP_VOXELMAN_LIGHT_H
 
+#include "core/version.h"
+
+#if VERSION_MAJOR < 4
 #include "scene/3d/spatial.h"
+#else
+#include "scene/3d/node_3d.h"
+
+#define Spatial Node3D
+#endif
 
 #include "core/math/vector3.h"
 
